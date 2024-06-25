5fa805e1878e287f3218d5962e4220ae8d2cd4de
Disaru Gunawardhana <disaru1314@gmail.com>
2024-06-25 15:52:26 +0530

    Update dashboard_page.dart

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
